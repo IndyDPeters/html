3c83567b935715c57ab2db92ec1c10b5151ff310
indypeters2006 <46998095+indypeters2006@users.noreply.github.com>
2021-12-03 11:13:37 +0100

    WIp
    Footer img fixed

---
+++ index.js
@@ -0,0 +1,21 @@
+function bigFooterImg(x) {
+    
+    x.style.width = "3%";
+  }
+
+  function smallFooterImg(x) {
+    
+    x.style.width = "2%";
+  }
+
+
+
+function bigFooterImg2(x) {
+    
+    x.style.width = "6%";
+  }
+
+  function smallFooterImg2(x) {
+    
+    x.style.width = "5%";
+  }
